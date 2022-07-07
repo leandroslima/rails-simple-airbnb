a59be18afa021f5137c25d2a872acd55ea4154d1
leandro lima <leandro.desouzalima1@gmail.com>
2022-07-07 12:32:13 -0300

    ground level

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+quality = ['Laid back', 'Clean & organized', 'Party-ready', 'Cozy']
+place = ['house', 'apartment', 'mansion', 'room']
+distance = ['near', 'in front of', 'beside', 'around']
+price = (50..200).to_a
+guests = (1..5).to_a
+
+4.times do
+  Flat.create!(
+    name: "#{quality.sample} #{place.sample} #{distance.sample} #{Faker::Address.community}",
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: price.sample,
+    number_of_guests: guests.sample
+  )
+end
