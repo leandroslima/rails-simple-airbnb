104b47b2413aa31b544a6815d8e29240ed82d078
leandro lima <leandro.desouzalima1@gmail.com>
2022-08-05 13:34:17 -0300

    photos correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-quality = ['Laid back', 'Clean & organized', 'Party-ready', 'Cozy']
+quality = ['Laid back', 'Clean & organized', 'Party-ready', 'Cozy', 'Relaxing', 'Quiet', 'Pet-friendly', 'Big']
 place = ['house', 'apartment', 'mansion', 'room']
 distance = ['near', 'in front of', 'beside', 'around']
 price = (50..200).to_a
 guests = (1..5).to_a
 
-4.times do
+12.times do
   Flat.create!(
     name: "#{quality.sample} #{place.sample} #{distance.sample} #{Faker::Address.community}",
     address: Faker::Address.full_address,
     description: Faker::Lorem.paragraph,
     price_per_night: price.sample,
     number_of_guests: guests.sample,
-    picture_url: "https://source.unsplash.com/1500x1000/?room"
+    picture_url: "https://source.unsplash.com/1500x1000/?room&sig=#{rand(300)}"
   )
 end
